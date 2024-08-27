fbb6b281899636d3798489bde1739df4574d9cd2
tristandm96 <tristandionnemichaud@gmail.com>
2024-08-27 14:30:22 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+// Mon premier server Http tristanS
+
+import http from 'http';
+import queryString from "query-string";
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    let reqInfo = { url: req.url, method: req.method, contentType: req.headers['content-type'] };
+    res.writeHead(200, { "Content-Type": "application/json" });
+    if (req.method == 'GET') {
+     res.end(JSON.stringify(reqInfo));
+    } else {
+     if (req.method == 'POST') {
+     let body = [];
+     req.on('data', chunk => {
+     body += chunk;
+     }).on('end', () => {
+     try {
+     if (req.headers['content-type'] === "application/json")
+     reqInfo.body = JSON.parse(body);
+     else
+     if (req.headers['content-type'] === "application/x-www-form-urlencoded")
+     reqInfo.body = queryString.parse(body.toString());
+     else
+     reqInfo.body = body.toString();
+     res.end(JSON.stringify(reqInfo));
+     } catch (error) {
+     console.log(error);
+     }
+     });
+     }
+    }
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
